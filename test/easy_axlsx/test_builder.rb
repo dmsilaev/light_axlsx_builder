70d868656f9dcb60901722459864e1d806cec3a3
Dmitry Silaev <dmsilaev@yandex.ru>
2014-11-12 22:56:38 +0400

    comment test builder

--- test/easy_axlsx/test_builder.rb
+++ test/easy_axlsx/test_builder.rb
@@ -7,6 +7,17 @@ class TestBuilder < Minitest::Test
     @created_object = EasyAxlsx::Builder.build @row_items
   end
 
+  # TODO: Realize functional
+  # def test_build_block
+  #   @row_items = (1...3).map { |i| ClassTest.new(id: i, name: "Name#{i}", last_name: "Last Name #{i}") }
+
+  #   a = EasyAxlsx::Builder.build do |f|
+  #     f.widths 1.5, 2.5, 5.5
+  #     f.headers "Id", "Name", "Last Name"
+  #     f.rows @row_items
+  #   end
+  # end
+
   def test_equal_rows
     package = Axlsx::Package.new
     building_object = EasyAxlsx::Building.new row_items: @row_items, package: package
